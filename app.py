7d743b3b0aa804382d0ad23e3b5d6b8e8ea10189
Sandesh-Nohara <122865900+Sandesh-Nohara@users.noreply.github.com>
2024-01-05 14:21:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,5 +26,5 @@ def predict_placement():
     return render_template('index.html',result=result)
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8080)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',port=8080)
